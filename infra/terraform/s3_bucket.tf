d878cdb596997c238dedf494cc3cf25a7c34b419
nothandonk <nothandonkambule1@gmail.com>
2024-09-02 12:46:31 +0200

    hotfix

--- infra/terraform/s3_bucket.tf
+++ infra/terraform/s3_bucket.tf
@@ -25,15 +25,3 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" 
     }
   }
 }
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks"
-  billing_mode = "PAY_PER_REQUEST"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  hash_key = "LockID"
-}
